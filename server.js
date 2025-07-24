00281a4526da3d3e634a8eba58f63b1efde0e334
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-07-24 15:29:50 +0000

    initial commit of init node and basic folder structuring

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const {MongoClient} = require('mongodb');
+
+//serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+a
