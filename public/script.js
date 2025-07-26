d074a30c1f22dbfaff142f88590042ae7bd70bfe
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-07-26 22:35:01 +0000

    update

--- public/script.js
+++ public/script.js
@@ -9,3 +9,52 @@ document.addEventListener('DOMContentLoaded', () => {
     }, index * 300); // I'M staggering animations by 300ms
   });
 });
+
+
+async function deleteContact(contactId) {
+    const confirmDelete = confirm('Are you sure you want to delete this contact?');
+    if (!confirmDelete) return;
+
+    try {
+        const response = await fetch(`https://cse341-project1-abgw.onrender.com/api/deleteContact/${contactId}`, {
+          method: 'DELETE'
+      });
+
+    if (response.status === 204) {
+        alert('Contact deleted successfully.');
+    } else {
+        const data = await response.json();
+        alert(`Failed to delete contact: ${data.error}`);
+      }
+      } catch (error) {
+        alert('Error deleting contact: ' + error.message);
+      }
+    }
+
+async function updateContact(contactId) {
+  const updatedData = {
+    name: "Updated Name",
+    email: "updated@example.com",
+    phone: "123-456-7890"
+  };
+
+  try {
+    const response = await fetch(`https://cse341-project1-abgw.onrender.com/api/updateContact/${contactId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+       body: JSON.stringify(updatedData)
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert('Contact updated successfully');
+    } else {
+      alert(`Failed to update contact: ${data.error}`);
+    }
+  } catch (error) {
+     alert('Error updating contact: ' + error.message);
+  }
+}
