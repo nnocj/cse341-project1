cc696e7ea02d9a4a9bf79f08bcb9d43dbc8086ad
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-07-26 07:07:48 +0000

    addding css and js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,11 @@
-const url = 'http://localhost:3000/api/customers';
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const fadeElements = document.querySelectorAll('.fade-in');
+
+  fadeElements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add('show');
+    }, index * 300); // I'M staggering animations by 300ms
+  });
+});
